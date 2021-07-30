d1f7c1bb2c18918373b2ca02698d00c60cfbe7a1
qtian13 <qiushuang.tian@gmail.com>
2021-07-30 09:45:45 -0700

    finish routes for tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,7 +52,8 @@ router.put('/:id', async (req, res) => {
     const categoryData = await Category.update(req.body, { where: {id: req.params.id} });
     
     if (!categoryData) {
-      res.status(404).json({message: 'No category found with that id!'})
+      res.status(404).json({message: 'No category found with that id!'});
+      return;
     }
 
     res.status(200).json(categoryData);
